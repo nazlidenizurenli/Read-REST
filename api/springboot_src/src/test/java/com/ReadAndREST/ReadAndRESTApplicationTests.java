d67f6894a237c146b548c8b3cf0c0dbb78fadb35
Nazli Urenli <nazlidenizurenli@berkeley.edu>
2024-07-09 11:34:33 -0700

    Spring boot works successfully.

--- api/springboot_src/src/test/java/com/ReadAndREST/ReadAndRESTApplicationTests.java
+++ api/springboot_src/src/test/java/com/ReadAndREST/ReadAndRESTApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.ReadAndREST;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+public class ReadAndRESTApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
+
