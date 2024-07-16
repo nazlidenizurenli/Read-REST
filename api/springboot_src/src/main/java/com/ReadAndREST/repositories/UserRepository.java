5c2f87e3f2b0c72273c9c4fdf6b31e2f29bddf92
Nazli Urenli <nazlidenizurenli@berkeley.edu>
2024-07-15 17:59:59 -0700

    Bookdatabase works.

--- api/springboot_src/src/main/java/com/ReadAndREST/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ReadAndREST.repositories;
-
-import com.ReadAndREST.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
